a89e32c0a810180c212a1b5bbabbea45fdeff5ee
Donny <donnygapulao@gmail.com>
2025-08-24 02:49:03 +0000

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import {defineConfig} from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        host: 'laratest.loc',
+        port: 5175,
+        strictPort: true,
+        origin: 'http://laratest.loc:5175',
+        cors: true,
+        hmr: {
+            port: 5175,
+            host: 'localhost',
+        }
+    },
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js', 'resources/css/app.css'],
+            refresh: true,
+        }),
+    ],
+});
